971bcaaaa9fdcd6544f1dc40fdbff58d632c2d5c
Daniël de Kok <me@danieldk.eu>
2023-12-10 09:05:27 +0100

    extend-pipeline: support extending before an existing pipe

--- spacy_nl_lemmatizer_ext/cli/extend_pipeline.py
+++ spacy_nl_lemmatizer_ext/cli/extend_pipeline.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
 import srsly
 from radicli import Arg
@@ -13,11 +13,14 @@ from . import cli
     model=Arg(help="spaCy model to extend"),
     lexicon=Arg(help="Lexicon file"),
     model_output=Arg(help="Extended spaCy model"),
+    before_pipe=Arg("--before", help="Pipe to prepend the gigant lemmatizer to"),
 )
-def extend_pipeline(model: str, lexicon: Path, model_output: Path):
+def extend_pipeline(
+    model: str, lexicon: Path, model_output: Path, before_pipe: Optional[str]
+):
     """Add the GiGaNT lemmatizer to a pipeline"""
     nlp = spacy.load(model)
     lexicon: Dict[str, Dict[str, str]] = srsly.read_json(lexicon)
-    lemmatizer = nlp.add_pipe("gigant_lemmatizer")
+    lemmatizer = nlp.add_pipe("gigant_lemmatizer", before=before_pipe)
     lemmatizer.initialize(lexicon=lexicon)
     nlp.to_disk(model_output)
